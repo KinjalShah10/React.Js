7685ad1278ea80522b398e4b36c59589b54995bf
KinjalShah10 <shahkinjal0813@gmail.com>
2024-05-10 21:10:52 +0530

    custom react added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,11 @@
 
 import Sample from "./sample"
 function App() {
+  const username = "Kinjal Shah"
 
   return (
     <>
-    <h1>HEY!!!! </h1>
+    <h1>HEY!!!! {username}</h1>
     <Sample/>
     </>
   )
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>
+        custom app
+      </h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props:{
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+const anotherUser = "HEloo User"
+const ReactElement = React.createElement(
+  'a',{href:"https://google.com",target:"_blank"},
+  "click me to visit google",
+  anotherUser
+)
+
+const anotherElement=(<a href='https://googl.com'target='_blank'>Visit Google</a>)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    //<App />
+    ReactElement
+    //anotherElement
+
+  //<MyApp /> can be return also as MyApp() bcz at end its the function and in javascript.
+  //</React.StrictMode>,
 )
