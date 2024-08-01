19cae5f50dd16ab5bc1655d41081d55529d6b980
KinjalShah10 <shahkinjal0813@gmail.com>
2024-08-01 20:05:07 +0530

    custom react added

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -7,19 +7,24 @@
 //     container.appendChild(domElement)
 // }
 
-
+//createElement is a method to create 
 const domElement=document.createElement(reactElement.type)
 domElement.innerHTML = reactElement.children
+
+//same only here we just took the for loop 
 for (const prop in reactElement.props) {
-    if (prop === 'children') continue;{
+    if (prop === 'children') continue;
+    {
         domElement.setAttribute(prop , reactElement.props[prop])
 
         
     }
     Container.appendChild(domElement)
 }
-//How react is shown after compiling
-const reactElement = {
+
+
+//How react is shown after compiling:
+const reactElement = {  //takes 3 things as a object => type, props,children => props is nothing but properties we give into object
     type: 'a',
     props:{
         href: 'https://google.com',
