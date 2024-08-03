813518d1301d1a3955388de3ab8e1e19166e19c4
KinjalShah10 <shahkinjal0813@gmail.com>
2024-08-03 16:51:24 +0530

    vite project added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,11 +3,12 @@ import Sample from "./sample"
 
 //app is just a function made.which will return the html.
 function App() {
-  const username = "Kinjal Shah"
+  const username = "Kinjal Shah" // expression
 
   return (
     <>
-    <h1>HEY!!!! {username}</h1>
+    <h1>HEY!!!! {username}</h1> 
+    {/* evaluated expression is written in {}*/}
     <Sample/>
     </>
   )
